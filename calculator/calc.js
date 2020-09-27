6511cb9bd00fc57394450e0d32e14f8a09af045a
Alec <57724438+archylex@users.noreply.github.com>
2020-09-27 13:26:07 +0300

    fix: fraction

--- calculator/calc.js
+++ calculator/calc.js
@@ -139,11 +139,11 @@ const fraction = (a, b, o) => {
     
     switch (o) {
         case '+':
-            return (Number(a) + Number(b)).toFixed(decA.length + decB.length).toString();                  
+            return Number((Number(a) + Number(b)).toFixed(decA.length + decB.length)).toString();                  
         case '-':
-            return (Number(a) + Number(b)).toFixed(decA.length + decB.length).toString();
+            return Number((Number(a) + Number(b)).toFixed(decA.length + decB.length)).toString();
         case '*':
-            return (Number(a) + Number(b)).toFixed(decA.length + decB.length).toString();
+            return Number((Number(a) + Number(b)).toFixed(decA.length + decB.length)).toString();
     }
 }
 
